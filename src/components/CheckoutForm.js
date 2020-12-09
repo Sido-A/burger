35f117de15110d3b14f11a6f479837cea7d736a4
Sido-A <sidonie.abe.uk@gmail.com>
2020-12-09 21:04:27 +0000

    added check out forms js file and functions

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "../css/CheckoutForm.css";
+import { burgersContext } from "../Context";
 
 function CheckoutForm() {
+  const { state, dispatch } = useContext(burgersContext);
+  const [input, setInput] = useState({});
+  const history = useHistory();
+
+  const changeHandler = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (input !== "") {
+      dispatch({
+        type: "ADD_USER",
+        payload: input,
+      });
+      history.replace("/orderComplete");
+    }
+  };
+
+  console.log(input);
   return (
-    <form className="checkoutForm">
+    <form className="checkoutForm" onSubmit={submitHandler}>
       <p className="checkoutForm__title">Your details</p>
-      <select name="genderTitle">
+      <select name="genderTitle" onChange={changeHandler}>
         <option value="Title" selected disabled hidden>
           Title*
         </option>
@@ -19,25 +44,51 @@ function CheckoutForm() {
           className="checkoutForm__firstName"
           type="text"
           placeholder="First Name*"
+          onChange={changeHandler}
+          name="firstName"
           required
         />
-        <input type="text" placeholder="Last Name*" required />
+        <input
+          type="text"
+          placeholder="Last Name*"
+          onChange={changeHandler}
+          name="lastName"
+          required
+        />
       </div>
-      <input type="email" placeholder="Email Address*" required />
-      <input type="password" placeholder="Password*" required />
-      <label for="mobileNumber">Contact number*</label>
+      <input
+        type="email"
+        placeholder="Email Address*"
+        onChange={changeHandler}
+        name="email"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password*"
+        onChange={changeHandler}
+        name="password"
+        required
+      />
+      <label htmlFor="mobileNumber">Contact number*</label>
       <input
         id="mobileNumber"
         type="text"
+        maxLength="10"
+        pattern="\d{10}"
         placeholder="Mobile Number*"
+        onChange={changeHandler}
+        name="mobileNumber"
         required
       />
-      <label for="postcode">Delivery Address*</label>
+      <label htmlFor="postcode">Delivery Address*</label>
       <input
         id="postcode"
         className="postcode"
         type="text"
         placeholder="Postcode*"
+        onChange={changeHandler}
+        name="postcode"
         required
       />
 
